304c4aeca82147aaeb961ba8d41703deb90fbe0e
daviaugusto222 <daviaugusto222@gmail.com>
2024-03-13 15:15:22 -0300

    Initial Commit

---
+++ testy/testyApp.swift
@@ -0,0 +1,17 @@
+//
+//  testyApp.swift
+//  testy
+//
+//  Created by user on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct testyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
