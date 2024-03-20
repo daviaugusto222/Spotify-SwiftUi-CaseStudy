93606817872a674700863383923da00499b603e3
daviaugusto222 <daviaugusto222@gmail.com>
2024-03-20 12:43:01 -0300

    Home with cards

---
+++ testy/LibraryView.swift
@@ -0,0 +1,10 @@
+//
+//  Library.swift
+//  testy
+//
+//  Created by user on 18/03/24.
+//
+
+import Foundation
+import SwiftUI
+
---
+++ testy/MainView.swift
@@ -0,0 +1,8 @@
+//
+//  MainView.swift
+//  testy
+//
+//  Created by user on 18/03/24.
+//
+
+import Foundation
